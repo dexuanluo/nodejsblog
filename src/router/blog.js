99a609e93d45e2b649693408bdc94802f949046d
Dexuan Luo <ldxkennedy333@gmail.com>
2020-07-11 15:14:21 -0500

    small fix

--- src/router/blog.js
+++ src/router/blog.js
@@ -1,23 +1,21 @@
 
 
 const handleBlogRouter = (req, res) =>{
-    const method = req.method;
-    const url = req.url;
-    const path = url.split('?')[0];
     
     
-    if (method === 'GET' && path === '/api/blog/list'){
+    
+    if (req.method === 'GET' && req.path === '/api/blog/list'){
         return {msg: 'blog list api'}
     };
 
-    if (method ==='GET' && path === '/api/blog/detail'){
+    if (req.method ==='GET' && req.path === '/api/blog/detail'){
         return {msg: 'blog detail api'}
     }
 
-    if (method ==='POST' && path === '/api/blog/new'){
+    if (req.method ==='POST' && req.path === '/api/blog/new'){
         return {msg: 'blog new api'}
     }
-    if (method ==='POST' && path === '/api/blog/del'){
+    if (req.method ==='POST' && req.path === '/api/blog/del'){
         return {msg: 'blog delete api'}
     }
 };
--- src/router/user.js
+++ src/router/user.js
@@ -1,11 +1,9 @@
 const handleUserRouter = (req, res) =>{
-    const method = req.method;
-    const url = req.url;
-    const path = url.split('?')[0];
+    
     
 
     //login
-    if (method === 'POST' && path === '/api/user/login'){
+    if (req.method === 'POST' && req.path === '/api/user/login'){
         return {msg: 'this is user login api'};
     }
 
