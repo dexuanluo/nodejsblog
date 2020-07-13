21f1695cf92b54a6831037cd46472cfb9c61f78d
Dexuan Luo <ldxkennedy333@gmail.com>
2020-07-13 17:41:29 -0500

    login cookie

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const  querrystring = require('querystring');
 const  handleBlogRouter = require('./src/router/blog');
 const  handleUserRouter = require('./src/router/user');
 
+
+const getCookieExp = ()=>{
+    const t = new Date(Date.now() + (24 * 60 * 60 * 1000));
+    return t.toUTCString();
+}
 const getPostData = (req)=>{
     const promise = new Promise(
         (resolve, reject) =>{
@@ -39,6 +44,7 @@ const getPostData = (req)=>{
     return promise;
 };
 
+const SESSION_CACHE = {};
 
 const serverHandler = (req, res)=> {
     //set header
@@ -48,16 +54,41 @@ const serverHandler = (req, res)=> {
     req.path = url.split('?')[0];
     //get query
     req.query = querrystring.parse(url.split('?')[1]);
+    //extract cookie
+    req.cookie = {};
+    let setCookie = false;
+    const cookieStr = req.headers.cookie||'';
+    
+    if (cookieStr === ''){setCookie = true;}
+    cookieStr.split(';').forEach((item)=>{
+        const arr = item.split('=');
+        const key = arr[0]||'';
+        const val = arr[1]||'';
+        req.cookie[key.trim()] = val.trim();
+    })
+    //handle session
+    const userId = req.cookie.userId || `${((Math.floor(Math.random()*10000) + Date.now()) - Math.floor(Math.random()*1000000))}_${Math.floor(Math.random()*1000000)}`;
+    
+    if (!SESSION_CACHE[userId]){
+        SESSION_CACHE[userId] = {};
+    }
+    req.session = SESSION_CACHE[userId]
     //deal with post req
     
     getPostData(req).then((postData) =>{
         req.body = postData;
         //handle router
+        
         const blogResult = handleBlogRouter(req, res);
         if (blogResult){
             blogResult.then(
                 (blogData)=>{
                     if (blogData){
+                        
+                        if (setCookie){
+                            
+                            res.setHeader(`Set-Cookie`, `userId=${userId}; path=/; httpOnly; expires=${getCookieExp()}`);
+                        }
                         res.end(
                             JSON.stringify(blogData)
                         );
@@ -74,6 +105,10 @@ const serverHandler = (req, res)=> {
             userResult.then(
                 (userData)=>{
                     if (userData){
+                        
+                        if (setCookie){
+                            res.setHeader(`Set-Cookie`, `userId=${userId}; path=/; httpOnly; expires=${getCookieExp()}`);
+                        }
                         res.end(
                             JSON.stringify(userData)
                         );
--- src/router/user.js
+++ src/router/user.js
@@ -1,16 +1,21 @@
 const {login_authentication} = require('../controller/user');
 const {SuccessRes, ErrorRes} = require('../model/result');
+
+
+
 const handleUserRouter = (req, res) =>{
     
     //login
-    if (req.method === 'POST' && req.path === '/api/user/login'){
-        const {username, password} = req.body;
+    if (req.method === 'GET' && req.path === '/api/user/login'){
+        const {username, password} = req.query;
         
         const result = login_authentication(username, password);
         return result.then(
             (data)=>{
                 if (data.length>0){
-                    return new SuccessRes();
+                    req.session.realname = data[0].realname;
+                    req.session.username = data[0].username;
+                    return new SuccessRes(req.session);
                 } else {
                     return new ErrorRes('wrong username or password');
                 }
@@ -18,6 +23,13 @@ const handleUserRouter = (req, res) =>{
         );
         
     }
+    if (req.method==='GET'&& req.path==='/api/user/login-test'){
+        if (req.cookie.userId){
+            return Promise.resolve(new SuccessRes(req.session));
+        }
+        return Promise.resolve(new ErrorRes('not login'));
+    }
+
 
 };
 
