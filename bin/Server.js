c058d99eac71c63a443a892791abb8915777945c
Dexuan Luo <ldxkennedy333@gmail.com>
2020-07-13 22:58:42 -0500

    quick fix

--- bin/Server.js
+++ bin/Server.js
@@ -3,4 +3,4 @@ const serverHandler = require('../app.js')
 const server = http.createServer(serverHandler);
 const port = 7000
 server.listen(port);
-console.log('Server OK, listening on ',port);
+console.log('Server OK, listening on ', port);
